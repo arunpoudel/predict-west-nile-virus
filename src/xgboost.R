6dc72533b0a328b72c0b984ebb7f23ab9725f484
Arun Poudel <arun@futurehome.no>
2015-05-25 21:07:24 +0200

    Improved xgboost
    Leaderboard Score: 0.74398

--- src/xgboost.R
+++ src/xgboost.R
@@ -1,9 +1,13 @@
 require(xgboost)
+require(caret)
+
+set.seed(555)
 
 print("Preparing Data")
 
 train <- read.csv('../Input/train.csv',header=TRUE,stringsAsFactors = F)
 test <- read.csv('../Input/test.csv',header=TRUE,stringsAsFactors = F)
+weather <- read.csv('../Input/weather.csv',header=TRUE,stringsAsFactors = F)
 
 #Shuffle
 train <- train[sample(nrow(train)),]
@@ -18,39 +22,29 @@ trainlength = nrow(train)
 
 x = rbind(train, test)
 
-#x$Year <- as.numeric(lapply(strsplit(x$Date, "-"), function(x) x[1]))
 x$Month <- as.numeric(lapply(strsplit(x$Date, "-"), function(x) x[2]))
 x$Week <- as.numeric(strftime(x$Date, format="%W"))
 
-x$month 			 <- x$Month
-#x$year               <- x$Year
-x$restuans           <- x$Species == 'CULEX RESTUANS'
-x$pipiens            <- x$Species == 'CULEX PIPIENS'
-x$both               <- x$Species == 'CULEX PIPIENS/RESTUANS'
-x$territans          <- x$Species == 'CULEX TERRITANS'
-x$latitude           <- x$Latitude
-x$longitude          <- x$Longitude
-x$block              <- x$Block
-
-x$Month <- NULL
-#x$Year <- NULL
 x$Date <- NULL
 x$NumMosquitos <- NULL
-x$Species <- NULL
+#x$Species <- NULL
 x$Latitude <- NULL
 x$Longitude <- NULL
 x$Address <- NULL
-x$Block <- NULL
+#x$Block <- NULL
 x$AddressNumberAndStreet <- NULL
-x$Trap <- NULL
-x$NumMosquitos <- NULL
 x$Street <- NULL
+x$AddressAccuracy <- NULL
 
-print(x[1:20,])
+dmy = dummyVars(" ~ . ", x)
 
-x = x[,-1]
+x <- data.frame(predict(dmy, newdata = x))
+
+headers = colnames(x)
 
 x = as.matrix(x)
+
+print(paste("scale_pos_weight=", sum(y==0) / sum(y==1)))
 
 print("Training the model")
 
@@ -64,13 +58,18 @@ param <- list("objective" = "binary:logistic",
               "min_child_weight" = 3,
               "subsample" = .9,
               "colsample_bytree" = .45,
-              "scale_pos_weight" = sum(y==0) / sum(y==1))
+              "scale_pos_weight" = sum(y==0) / sum(y==1),
+              "base_score" = "0.7")
 
-nround = 200
-bst = xgboost(param=param, data = x[1:trainlength,], label = y, nrounds=nround, verbose = 2)
+nround = 500
+bst = xgboost(param=param, data = x[1:trainlength,], label = y, nrounds=nround, verbose = 0)
+
+importance_matrix <- xgb.importance(headers, model = bst)
+xgb.plot.importance(importance_matrix)
 
 print("Making prediction")
 pred = predict(bst, x[(nrow(train)+1):nrow(x),])
+summary(pred)
 pred = matrix(pred,1,length(pred))
 pred = t(pred)
 
@@ -78,4 +77,4 @@ print("Storing Output")
 pred = format(pred, digits=2,scientific=F) # shrink the size of submission
 pred = data.frame(1:nrow(pred), pred)
 names(pred) = c('Id', 'WnvPresent')
-write.csv(pred, file="../Output/nile-river.csv", quote=FALSE,row.names=FALSE)
+write.csv(pred, file="../Output/nile-river-boost.csv", quote=FALSE,row.names=FALSE)
