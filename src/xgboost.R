1446384a5f852759b98053276b0fcd99f5341471
Arun Poudel <arun@futurehome.no>
2015-05-29 03:45:32 +0200

    joined weather data
    LB Performance is bad, but we will get to that later

--- src/xgboost.R
+++ src/xgboost.R
@@ -1,13 +1,35 @@
 require(xgboost)
 require(caret)
+library(plyr)
 
-set.seed(2000)
+set.seed(1000)
 
 print("Preparing Data")
 
 train <- read.csv('../Input/train.csv',header=TRUE,stringsAsFactors = F)
 test <- read.csv('../Input/test.csv',header=TRUE,stringsAsFactors = F)
 weather <- read.csv('../Input/weather.csv',header=TRUE,stringsAsFactors = F)
+
+weather$CodeSum <- NULL
+weather$Water1 <- NULL
+weather$SnowFall <- NULL
+weather$Depth <- NULL
+weather$CodeSum <- NULL
+weather$Sunrise <- NULL
+weather$Sunset <- NULL
+weather[weather == 'M'] <- -1
+weather[weather == '-'] <- -1
+weather[weather == 'T'] <- -1
+weather[weather == ' T'] <- -1
+weather[weather == '  T'] <- -1
+
+weather_station1 <- weather[weather$Station == 1,]
+weather_station1$Station <- NULL
+weather_station2 <- weather[weather$Station == 2,]
+weather_station2$Station <- NULL
+
+weather = join(weather_station1, weather_station2, by = 'Date')
+weather[is.character(weather)] <- -1
 
 #Shuffle
 train <- train[sample(nrow(train)),]
@@ -28,18 +50,30 @@ x$Week <- as.numeric(strftime(x$Date, format="%W"))
 
 x$Latitude <- NULL
 x$Longitude <- NULL
-x$Date <- NULL
 x$AddressNumberAndStreet <- NULL
 x$Street <- NULL
 x$AddressAccuracy <- NULL
 
-dmy = dummyVars(" ~ . ", x)
+date <- x$Date
+x$Date <- NULL
 
+dmy = dummyVars(" ~ . ", x)
 x <- data.frame(predict(dmy, newdata = x))
+
+x$Date <- date
+x = join(x, weather, by = 'Date')
+
+x[is.na(x)] <- -1
+
+x$Date <- NULL
 
 headers = colnames(x)
 
 x = as.matrix(x)
+
+mode(x) <- "numeric"
+
+print(head)
 
 print(paste("scale_pos_weight=", sum(y==0) / sum(y==1)))
 
@@ -67,8 +101,13 @@ xgb.plot.importance(importance_matrix)
 
 print("Making prediction")
 pred = predict(bst, x[(nrow(train)+1):nrow(x),])
+#for(i in 1:length(pred)) {
+       #pred[[i]] = ifelse(pred[[i]] < 0.2, 0, pred[[i]])
+       #pred[[i]] = ifelse(pred[[i]] > 0.8, 1, pred[[i]])
+#}
 summary(pred)
 pred = matrix(pred,1,length(pred))
+
 pred = t(pred)
 
 print("Storing Output")
